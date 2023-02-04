ce002e470eab6a926b6e2cda99c5c91f4a9c6ffa
Benjamin Janssens <benji.janssens@gmail.com>
2023-02-04 21:45:41 +0100

    Converted Jest config to JSON

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    clearMocks: true,
-    coverageProvider: 'v8',
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/src'],
-};
