0254364f5a2d25c34d2e5205d7599366f4edc3bb
Tom De Buyser <tom.debuyser@icapps.com>
2023-02-15 14:01:48 +0100

    first cleanup

--- src/get-env.ts
+++ src/get-env.ts
@@ -1,62 +1,48 @@
-import { exec } from 'child_process';
 import fs from 'fs';
+import { item, validateCli, NotesField } from '@1password/op-js';
 
-export type OpItem = {
-    id: string;
-    title: string;
-    version: number;
-    vault: Vault;
-    category: string;
-    last_edited_by: string;
-    created_at: Date;
-    updated_at: Date;
-    sections: Section[];
-    fields: Field[];
-};
-
-export type Field = {
-    id: string;
-    type: string;
-    purpose?: string;
-    label: string;
-    reference: string;
-    section?: Section;
-    value?: string;
-};
-
-export type Section = {
-    id: string;
-};
-
-export type Vault = {
-    id: string;
-    name: string;
-};
+validateCli().catch((error) => {
+    // TODO: Stop execution of command
+    console.error('CLI is not valid:', error.message);
+});
 
 // first two args are node and the script name
+// TODO: param names
 const [itemNameOrId, environment] = process.argv.slice(2);
 
-exec(`op item get ${itemNameOrId} --format json`, (err, stdout, stderr) => {
-    if (err) console.log(`err`, err);
-    if (stderr) console.log(`stderr`, stderr);
+// Get Secure Note from 1password
+const response = item.get(itemNameOrId);
+const { values, secrets } = (response.fields || []).reduce<{
+    values: Record<string, string>;
+    secrets: Record<string, string>;
+}>(
+    (acc, curr) => {
+        if ((curr as NotesField).purpose) return acc;
+        return {
+            values: { ...acc.values, [curr.label]: curr.value },
+            secrets: { ...acc.secrets, [curr.label]: curr.reference! },
+        };
+    },
+    {
+        values: {},
+        secrets: {},
+    }
+);
 
-    const fullItem: OpItem = JSON.parse(stdout);
+// Create .env<.environment> file with all plain values
+fs.writeFile(
+    `.env${environment ? `.${environment}` : ''}`,
+    Object.keys(values)
+        .map((key) => `${key}=${values[key]}`)
+        .join('\n'),
+    () => {}
+);
 
-    const valueFile = fullItem.fields
-        .filter((field) => !field.purpose)
-        .map((field) => `${field.label}=${field.value}`);
-    const secretFile = fullItem.fields
-        .filter((field) => !field.purpose)
-        .map((field) => `${field.label}=${field.reference}`);
-
-    fs.writeFile(
-        `.env${environment ? `.${environment}` : ''}`,
-        valueFile.join('\n'),
-        () => {}
-    );
-    fs.writeFile(
-        `.env.secret${environment ? `.${environment}` : ''}`,
-        secretFile.join('\n'),
-        () => {}
-    );
-});
+// Create .env.secrets<.environment> file with all references to 1password entries
+fs.writeFile(
+    `.env.secret${environment ? `.${environment}` : ''}`,
+    Object.keys(secrets)
+        .map((key) => `${key}=${secrets[key]}`)
+        .join('\n'),
+    () => {}
+);
